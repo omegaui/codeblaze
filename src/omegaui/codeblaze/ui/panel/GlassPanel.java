00384c093600c9103a3cec47f70ab65e6addea2b
omegaui <omegaui22@gmail.com>
2022-05-03 21:49:32 +0530

    Added LauncherPanel, Extended GlassPanel Behaviour!

--- src/omegaui/codeblaze/ui/panel/GlassPanel.java
+++ src/omegaui/codeblaze/ui/panel/GlassPanel.java
@@ -6,6 +6,7 @@ import omegaui.component.TextComp;
 import java.awt.Graphics;
 import java.awt.Graphics2D;
 import java.awt.RenderingHints;
+import java.awt.BorderLayout;
 
 import omegaui.codeblaze.App;
 
@@ -15,71 +16,25 @@ import static omegaui.codeblaze.io.UIXManager.*;
 import static omegaui.codeblaze.io.AppDataProvider.*;
 import static omegaui.component.animation.Animations.*;
 
-public class GlassPanel extends JPanel implements ResizeAware{
+public final class GlassPanel extends JPanel {
 	private App app;
 
-	private TextComp appIconComp;
-	private TextComp appTitleComp;
-	private TextComp appVersionComp;
+	// Views
+	private LauncherPanel launcherPanel;
 
-	// Actions
-
-	private TextComp createFileComp;
-	private TextComp openFileComp;
-	
 	public GlassPanel(App app){
+		super(new BorderLayout());
 		this.app = app;
 		initUI();
 	}
 
 	public void initUI(){
-		setBackground(BACKGROUND);
-		
-		appIconComp = new TextComp(appIcon, BACKGROUND, BACKGROUND, BACKGROUND, null);
-		appIconComp.setClickable(false);
-		appIconComp.setArc(0, 0);
-		add(appIconComp);
-
-		appTitleComp = new TextComp("CodeBlaze", BACKGROUND, BACKGROUND, GLOW, null);
-		appTitleComp.setFont(PX44);
-		appTitleComp.setClickable(false);
-		appTitleComp.setArc(0, 0);
-		add(appTitleComp);
-
-		appVersionComp = new TextComp(appVersionSemantic(), BACKGROUND, BACKGROUND, GLOW, null);
-		appVersionComp.setFont(PX28);
-		appVersionComp.setClickable(false);
-		appVersionComp.setArc(0, 0);
-		add(appVersionComp);
-
-		initActions();
+		launcherPanel = new LauncherPanel(app);
+		putToView(launcherPanel);
 	}
 
-	public void initActions(){
-		createFileComp = new TextComp("Create New File", HOVER, BACKGROUND, tertiaryColor, null);
-		createFileComp.setFont(PX18.bold());
-		createFileComp.setArc(6, 6);
-		add(createFileComp);
-		
-		openFileComp = new TextComp("Open a Local File", HOVER, BACKGROUND, tertiaryColor, null);
-		openFileComp.setFont(PX18.bold());
-		openFileComp.setArc(6, 6);
-		add(openFileComp);
-	}
-
-	@Override
-	public void manageBounds() {
-		appIconComp.setBounds(getWidth()/2 - 200/2, getHeight()/2 - 400/2, 200, 200);
-		appTitleComp.setBounds(getWidth()/2 - 200/2, appIconComp.getY() + appIconComp.getHeight(), 200, 60);
-		appVersionComp.setBounds(getWidth()/2 - 150/2, appTitleComp.getY() + appTitleComp.getHeight(), 150, 30);
-
-		createFileComp.setBounds(getWidth()/2 - 250/2, getHeight() - 150, 250, 30);
-		openFileComp.setBounds(getWidth()/2 - 250/2, createFileComp.getY() + createFileComp.getHeight() + 5, 250, 30);
-	}
-
-	@Override
-	public void layout(){
-		super.layout();
-		manageBounds();
+	public void putToView(JPanel panel){
+		removeAll();
+		add(panel, BorderLayout.CENTER);
 	}
 }
