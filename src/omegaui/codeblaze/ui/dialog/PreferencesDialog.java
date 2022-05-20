be1872f268b29ec30db8a638c0480a816b07950a
omegaui <omegaui22@gmail.com>
2022-05-20 23:01:03 +0530

    Imported omegaui-library dailybuild!

--- src/omegaui/codeblaze/ui/dialog/PreferencesDialog.java
+++ src/omegaui/codeblaze/ui/dialog/PreferencesDialog.java
@@ -1,10 +1,13 @@
 package omegaui.codeblaze.ui.dialog;
+import java.awt.Dimension;
+
 import omegaui.codeblaze.App;
 
 import omegaui.component.TextComp;
 
 import javax.swing.JDialog;
 import javax.swing.JPanel;
+import javax.swing.BoxLayout;
 
 import static omegaui.codeblaze.io.UIXManager.*;
 import static omegaui.codeblaze.io.AppDataProvider.*;
@@ -17,6 +20,9 @@ public class PreferencesDialog extends JDialog{
 	private TextComp iconComp;
 	private TextComp titleComp;
 	private TextComp closeComp;
+
+	private JPanel panel;
+	private TextComp autoSaveFileOnExitLabel;
 	
 	public PreferencesDialog(App app){
 		super(app, true);
@@ -31,9 +37,9 @@ public class PreferencesDialog extends JDialog{
 	}
 
 	public void initUI(){
-		JPanel panel = new JPanel(null);
-		setContentPane(panel);
-		panel.setBackground(back3);
+		JPanel contentPane = new JPanel(null);
+		setContentPane(contentPane);
+		contentPane.setBackground(back3);
 		
 		iconComp = new TextComp(gearIcon, 48, 48, back3, back3, back3, null);
 		iconComp.setBounds(0, 0, 50, 50);
@@ -56,5 +62,19 @@ public class PreferencesDialog extends JDialog{
 		add(closeComp);
 
 		putAnimationLayer(closeComp, getImageSizeAnimationLayer(20, +5, true), ACTION_MOUSE_ENTERED);
+
+		panel = new JPanel();
+		panel.setLayout(new BoxLayout(panel, BoxLayout.PAGE_AXIS));
+		panel.setBounds(5, 60, getWidth() - 10, getHeight() - 70);
+		panel.setBackground(back3);
+		panel.setAlignmentX(JPanel.CENTER_ALIGNMENT);
+		add(panel);
+
+		autoSaveFileOnExitLabel = new TextComp("Auto Save Files on Exit", "Enabling this feature will Silently Save Files without letting prompting any choice dialog.", back3, back3, GLOW, null);
+		autoSaveFileOnExitLabel.setPreferredSize(new Dimension(250, 30));
+		autoSaveFileOnExitLabel.setFont(PX16);
+		autoSaveFileOnExitLabel.setClickable(false);
+		autoSaveFileOnExitLabel.setArc(0, 0);
+		panel.add(autoSaveFileOnExitLabel);
 	}
 }
